1018d386d01b8ee6ba8312fdc779a06f81449370
R. Lemos <rslemos@gmail.com>
2013-08-22 17:20:59 -0400

    PIC P(n)

--- src/test/java/br/eti/rslemos/cobolg/LexerPICTURECLAUSEUnitTest.java
+++ src/test/java/br/eti/rslemos/cobolg/LexerPICTURECLAUSEUnitTest.java
@@ -20,6 +20,7 @@ public class LexerPICTURECLAUSEUnitTest extends TestCase {
 			"PIC S9(8)",
 			"PIC S9(12)V9(5)",
 			"PIC 9(3)9(6)",
+			"PIC 9(3)P(6)",
 		};
 
 	public LexerPICTURECLAUSEUnitTest(String name) {
