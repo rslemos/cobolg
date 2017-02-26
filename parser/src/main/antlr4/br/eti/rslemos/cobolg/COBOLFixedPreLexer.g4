9dcdf24f171185200aa5aa785368c980fa9518a7
R. Lemos <rslemos@gmail.com>
2017-02-26 13:48:31 -0300

    new pre lexer for fixed format (avoid marks)

---
+++ parser/src/main/antlr4/br/eti/rslemos/cobolg/COBOLFixedPreLexer.g4
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * BEGIN COPYRIGHT NOTICE
+ * 
+ * This file is part of program "cobolg"
+ * Copyright 2017  Rodrigo Lemos
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * END COPYRIGHT NOTICE
+ ******************************************************************************/
+lexer grammar COBOLFixedPreLexer;
+
+// ugly ANTLR4 syntax for lexers will not allow for ranges
+
+fragment NON_NEWLINE : ~[\n\r];
+
+// these will encode exact ranges: {n}
+fragment NON_NEWLINE_2 : NON_NEWLINE NON_NEWLINE;
+fragment NON_NEWLINE_4 : NON_NEWLINE_2 NON_NEWLINE_2;
+fragment NON_NEWLINE_8 : NON_NEWLINE_4 NON_NEWLINE_4;
+fragment NON_NEWLINE_16 : NON_NEWLINE_8 NON_NEWLINE_8;
+fragment NON_NEWLINE_32 : NON_NEWLINE_16 NON_NEWLINE_16;
+fragment NON_NEWLINE_64 : NON_NEWLINE_32 NON_NEWLINE_32;
+
+fragment NON_NEWLINE_6 : NON_NEWLINE_4 NON_NEWLINE_2;
+fragment NON_NEWLINE_65 : NON_NEWLINE_64 NON_NEWLINE;
+
+// mode DEFAULT_MODE;
+NEWLINE : ('\n' '\r'? | '\r' '\n'?)
+	;
+
+SEQUENCE_NUMBER : NON_NEWLINE_6
+	-> mode(INDICATOR_MODE)
+	;
+
+mode INDICATOR_MODE;
+INDICATOR_CODE : ' '
+	-> mode(SOURCE_MODE)
+	;
+
+INDICATOR_DEBUG : 'D'
+	-> mode(SOURCE_MODE)
+	;
+
+INDICATOR_COMMENT : ('*' | '/')
+	-> mode(SOURCE_MODE)
+	;
+
+INDICATOR_CONTINUATION : '-'
+	-> mode(SOURCE_MODE)
+	;
+
+INDICATOR_ELSE : NON_NEWLINE
+	-> mode(SOURCE_MODE)
+	;
+
+mode SOURCE_MODE;
+SOURCE : NON_NEWLINE_65
+	-> mode(TAIL_MODE)
+	;
+
+mode TAIL_MODE;
+TAIL : NON_NEWLINE*
+	-> mode(DEFAULT_MODE)
+	;
+
---
+++ parser/src/test/java/br/eti/rslemos/cobolg/FixedFormatPreLexerUnitTest.java
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * BEGIN COPYRIGHT NOTICE
+ * 
+ * This file is part of program "cobolg"
+ * Copyright 2017  Rodrigo Lemos
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * END COPYRIGHT NOTICE
+ ******************************************************************************/
+package br.eti.rslemos.cobolg;
+
+import static br.eti.rslemos.cobolg.COBOLFixedPreLexer.*;
+import static br.eti.rslemos.cobolg.TextHelper.join;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.TokenSource;
+import org.junit.Test;
+
+public class FixedFormatPreLexerUnitTest extends AbstractLexerUnitTest {
+
+	@Test
+	public void testEmptyLine() throws Exception {
+		setSource("                                                                                ");
+		
+		matchToken(SEQUENCE_NUMBER,        "      ");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "                                                                 ");
+		matchToken(TAIL,                   "        ");
+		
+		matchEOF();
+	}
+
+	@Test
+	public void testSequenceAreaLine() throws Exception {
+		setSource("012345                                                                          ");
+		
+		matchToken(SEQUENCE_NUMBER,        "012345");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "                                                                 ");
+		matchToken(TAIL,                   "        ");
+		
+		matchEOF();
+	}
+
+	@Test
+	public void testSkipToEolLine() throws Exception {
+		setSource("                                                                        IGNORED+");
+		
+		matchToken(SEQUENCE_NUMBER,        "      ");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "                                                                 ");
+		matchToken(TAIL,                   "IGNORED+");
+		
+		matchEOF();
+	}
+
+	@Test
+	public void testCommentLine() throws Exception {
+		setSource("000001*THIS IS A COMMENT LINE                                           IGNORED+");
+		
+		matchToken(SEQUENCE_NUMBER,        "000001");
+		matchToken(INDICATOR_COMMENT,      "*");
+		matchToken(SOURCE,                 "THIS IS A COMMENT LINE                                           ");
+		matchToken(TAIL,                   "IGNORED+");
+		
+		matchEOF();
+	}
+	
+	@Test
+	public void testInlineComment() throws Exception {
+		setSource("000001 X. *> INLINE COMMENT UNTIL END OF AREA B                         IGNORED+");
+		
+		matchToken(SEQUENCE_NUMBER,        "000001");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "X. *> INLINE COMMENT UNTIL END OF AREA B                         ");
+		matchToken(TAIL,                   "IGNORED+");
+		
+		matchEOF();
+	}
+
+	@Test
+	public void testDoubleQuotedStart() throws Exception {
+		setSource("000000 \"DOUBLE QUOTED START                                             IGNORED+");
+		
+		matchToken(SEQUENCE_NUMBER,        "000000");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "\"DOUBLE QUOTED START                                             ");
+		matchToken(TAIL,                   "IGNORED+");
+		
+		matchEOF();
+	}
+
+
+	@Test
+	public void testDoubleQuotedWithDoubleQuotesStart() throws Exception {
+		setSource("000000 \"DOUBLE QUOTED START WITH \"\"DOUBLE QUOTES\"\"                      IGNORED+");
+		
+		matchToken(SEQUENCE_NUMBER,        "000000");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "\"DOUBLE QUOTED START WITH \"\"DOUBLE QUOTES\"\"                      ");
+		matchToken(TAIL,                   "IGNORED+");
+		
+		matchEOF();
+	}
+
+	@Test
+	public void testDoubleQuoteContinuationString() throws Exception {
+		setSource(join(
+			"000000 \"DOUBLE QUOTED START WITH \"\"DOUBLE QUOTES\"\" THAT LEAKS INTO THE NIGNORED+",
+			"000001-    \"EXT LINE\"                                                   IGNORED+"
+		));
+		
+		// 1st line
+		matchToken(SEQUENCE_NUMBER,        "000000");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "\"DOUBLE QUOTED START WITH \"\"DOUBLE QUOTES\"\" THAT LEAKS INTO THE N");
+		matchToken(TAIL,                   "IGNORED+");
+		matchToken(NEWLINE,                "\n");
+		
+		// 2nd line
+		matchToken(SEQUENCE_NUMBER,        "000001");
+		matchToken(INDICATOR_CONTINUATION, "-");
+		matchToken(SOURCE,                 "    \"EXT LINE\"                                                   ");
+		matchToken(TAIL,                   "IGNORED+");
+		
+		matchEOF();
+	}
+
+	@Test
+	public void testDoubleQuoteContinuationStringIn3Lines() throws Exception {
+		setSource(join(
+			"000000 \"DOUBLE QUOTED START WITH \"\"DOUBLE QUOTES\"\" THAT LEAKS INTO THE NIGNORED+",
+			"000001-    \"EXT LINE BUT KEEPS GOING OVER AND OVER AND OVER AND OVER ANDIGNORED+",
+			"000002-    \"OVER AGAIN UNTIL THE 3RD LINE\"                              IGNORED+"
+		));
+		
+		// 1st line
+		matchToken(SEQUENCE_NUMBER,        "000000");
+		matchToken(INDICATOR_CODE,         " ");
+		matchToken(SOURCE,                 "\"DOUBLE QUOTED START WITH \"\"DOUBLE QUOTES\"\" THAT LEAKS INTO THE N");
+		matchToken(TAIL,                   "IGNORED+");
+		matchToken(NEWLINE,                "\n");
+		
+		// 2nd line
+		matchToken(SEQUENCE_NUMBER,        "000001");
+		matchToken(INDICATOR_CONTINUATION, "-");
+		matchToken(SOURCE,                 "    \"EXT LINE BUT KEEPS GOING OVER AND OVER AND OVER AND OVER AND");
+		matchToken(TAIL,                   "IGNORED+");
+		matchToken(NEWLINE,                "\n");
+		
+		// 3rd line
+		matchToken(SEQUENCE_NUMBER,        "000002");
+		matchToken(INDICATOR_CONTINUATION, "-");
+		matchToken(SOURCE,                 "    \"OVER AGAIN UNTIL THE 3RD LINE\"                              ");
+		matchToken(TAIL,                   "IGNORED+");
+		
+		matchEOF();
+	}
+
+	@Override
+	protected TokenSource getLexer(Reader reader) throws IOException {
+		return new COBOLFixedPreLexer(new ANTLRInputStream(reader));
+	}
+}
